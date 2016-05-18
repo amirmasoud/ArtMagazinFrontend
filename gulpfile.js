f4e63fe9303865dc4ee1de6f2275612469026829
AmirMasoud Sheidayi <amirmasood33@gmail.com>
2016-05-18 14:23:17 +0430

    initial components and gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,97 @@
+var del = require('del');
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var gulpIf = require('gulp-if');
+var cache = require('gulp-cache');
+var image = require('gulp-image');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
+/**
+ * Development Tasks 
+ */
 
-});
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/assets/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass()) // Passes it through a gulp-sass
+    .pipe(gulp.dest('app/assets/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('app/assets/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/assets/js/**/*.js', browserSync.reload);
+})
+
+/**
+ * Optimization Tasks 
+ */
+
+// Optimizing CSS and JavaScript 
+gulp.task('useref', function() {
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+})
+
+// Optimizing Images 
+gulp.task('images', function() {
+  gulp.src('app/assets/img/**/*')
+  .pipe(image())
+  .pipe(gulp.dest('dist/img'))
+})
+
+// Copying fonts 
+gulp.task('fonts', function() {
+  return gulp.src('app/assets/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+})
+
+// Cleaning 
+gulp.task('clean', function() {
+  return del.sync('dist').then(function(cb) {
+    return cache.clearAll(cb);
+  });
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+})
+
+/**
+ * Build Sequences
+ */
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync', 'watch'],
+    callback
+  )
+})
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+})
