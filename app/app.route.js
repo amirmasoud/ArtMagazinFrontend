26b7d6f76cf5c230206a04f3d9b6e7e7d50a29ba
AmirMasoud Sheidayi <amirmasood33@gmail.com>
2016-05-31 13:16:06 +0430

    fix # problem in url with AngularJS

--- app/app.route.js
+++ app/app.route.js
@@ -5,12 +5,17 @@ angular
 	.module('app')
 	.config(config);
 
-config.$inject = ['$routeProvider'];
-function config($routeProvider) {
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: 'partials/index.html',
 			controller: 'ImageController'
 		});
+
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
 }
 
